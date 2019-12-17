91936ac9cd348eb3d4d7612b5c1d07cafd5953bf
Lydia B <lydiabaik@gmail.com>
2019-12-17 04:20:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,13 @@ new_array = []
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
+  puts badges
+  puts new_array
+  badges_and_room_assignments.each_line
 end
+
+#Now you have to tell the printer what to print.
+#Create a method called printer that will output first the results of the batch_badge_creator method
+#and then of the assign_rooms method to the screen.
 
 assign_rooms(["Kevin", "Aiden", "Chris"])
