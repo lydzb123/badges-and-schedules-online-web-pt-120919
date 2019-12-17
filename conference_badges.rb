914ab1cced7ffb39fc8203195d702e61c74a5f6f
Lydia B <lydiabaik@gmail.com>
2019-12-17 04:23:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,9 +16,22 @@ new_array = []
 end
 
 def printer(attendees)
-  puts badges
-  puts new_array
+
+  def batch_badge_creator(attendees)
+    badges = []
+    attendees.each{|name| badges << "Hello, my name is #{name}."}
+    return badges
+  end
+  def assign_rooms(attendees)
+  new_array = []
+    attendees.each_with_index{|name, index|
+      new_array << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+    return new_array
+  end
+  
 end
+
+
 
 #Now you have to tell the printer what to print.
 #Create a method called printer that will output first the results of the batch_badge_creator method
