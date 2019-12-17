8f7d9df95e84e3dc05c7834dfafca00a97fb0be8
Lydia B <lydiabaik@gmail.com>
2019-12-17 04:23:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,18 +17,10 @@ end
 
 def printer(attendees)
 
-  def batch_badge_creator(attendees)
-    badges = []
-    attendees.each{|name| badges << "Hello, my name is #{name}."}
-    return badges
-  end
-  def assign_rooms(attendees)
-  new_array = []
-    attendees.each_with_index{|name, index|
-      new_array << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
-    return new_array
-  end
+  assign_rooms.chomp
   
+  end
+
 end
 
 
